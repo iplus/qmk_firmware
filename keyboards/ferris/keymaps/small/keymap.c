d06ab7c9860add5904512ef23f816d42a0f071aa
ivan.plyusnin@gmail.com <Ivan Plyusnin>
2022-07-13 22:18:52 +0200

    add back compat F layer

--- keyboards/ferris/keymaps/small/keymap.c
+++ keyboards/ferris/keymaps/small/keymap.c
@@ -115,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_FN2] = LAYOUT( 
     KC_F11 , KC_F7  , KC_F8  , KC_F9  , XXXXXXX,      XXXXXXX, _______, _______, _______, _______,
     KC_F10 , KC_F4  , KC_F5  , KC_F6  , XXXXXXX,      XXXXXXX, _______, _______, _______, _______,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, KC_F1  , KC_F2  , KC_F3  , XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                  KC_LSFT, KC_LGUI,  KC_LALT, KC_LCTL
   ),
 };
