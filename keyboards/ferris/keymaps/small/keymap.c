fe50dd4ea8d14b9857bf2550296c44d54cc9ce88
WIN-L7GGTG8JIF4\iplus <iplus@mail.ru>
2024-01-24 13:04:21 +0200

    up

--- keyboards/ferris/keymaps/small/keymap.c
+++ keyboards/ferris/keymaps/small/keymap.c
@@ -109,13 +109,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_GAME] = LAYOUT(
     KC_LCTL, KC_Q, KC_W, KC_E, KC_T,              KC_I, KC_G,    KC_UP  , KC_B    , KC_ESC,
     KC_LSFT, KC_A, KC_S, KC_D, KC_G,              KC_H, KC_LEFT, KC_DOWN, KC_RIGHT, KC_ENT,
-    KC_LGUI, KC_Z, KC_X, KC_C, KC_V,              KC_N, KC_Y,    KC_M,    KC_J ,    KC_O,
+    KC_LGUI, KC_Z, KC_X, KC_C, KC_B,              KC_N, KC_Y,    KC_M,    KC_J ,    KC_O,
                             LT(_NMG, KC_R), KC_SPACE,  KC_LSFT, KC_LGUI
+  ),
+  //
+  [_NUM] = LAYOUT(
+    _______, _______, KC_LCTL, KC_LALT, XXXXXXX,      XXXXXXX, DV_4   , DV_5   , DV_6   , DV_8   ,
+    KC_PDOT, KC_PDOT, KC_LCTL, KC_LALT, XXXXXXX,      XXXXXXX, DV_1   , DV_2   , DV_3   , DV_7   ,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                                 KC_LGUI, KC_LSFT,    DV_0, DV_9
+  ),
+  [_FUN] = LAYOUT(
+    _______, _______, KC_LCTL, KC_LALT, XXXXXXX,      XXXXXXX, KC_F4  , KC_F5  , KC_F6  , KC_F8  ,
+    _______, _______, KC_LCTL, KC_LALT, XXXXXXX,      XXXXXXX, KC_F1  , KC_F2  , KC_F3  , KC_F7  ,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                                 KC_LGUI, KC_LSFT,    KC_F10, KC_F9
   ),
   [_NMG] = LAYOUT(
     KC_LGUI, KC_1 , KC_2,  KC_3,    KC_ESC,      XXXXXXX, _______, KC_VOLU, _______, TO(_AL1),
-    KC_TAB , KC_C , KC_X,  KC_F,    KC_B,        XXXXXXX, KC_MPLY, KC_VOLD, KC_MNXT, _______,
-    KC_M,    KC_F5, KC_F6, KC_Y,    KC_ENT,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    KC_TAB , KC_C , KC_X,  KC_F,    KC_ENT,      XXXXXXX, KC_MPLY, KC_VOLD, KC_MNXT, _______,
+    KC_M,    KC_F5, KC_F6, KC_Y,    KC_V  ,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                  _______, _______,  _______, _______
   ),
 /*
@@ -137,19 +150,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     DV_COLN, DV_DLR , _______, DV_TILD, XXXXXXX,      XXXXXXX, DV_DLR , DV_EXLM, DV_AMPR, DV_BSLS,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                  KC_LGUI, KC_LSFT,  KC_LCTL, KC_LALT
-  ),
- //
-  [_NUM] = LAYOUT(
-    _______, _______, KC_LCTL, KC_LALT, XXXXXXX,      XXXXXXX, DV_4   , DV_5   , DV_6   , DV_8   ,
-    KC_PDOT, KC_PDOT, KC_LCTL, KC_LALT, XXXXXXX,      XXXXXXX, DV_1   , DV_2   , DV_3   , DV_7   ,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                 KC_LGUI, KC_LSFT,    DV_0, DV_9
-  ),
-  [_FUN] = LAYOUT(
-    _______, _______, KC_LCTL, KC_LALT, XXXXXXX,      XXXXXXX, KC_F4  , KC_F5  , KC_F6  , KC_F8  ,
-    _______, _______, KC_LCTL, KC_LALT, XXXXXXX,      XXXXXXX, KC_F1  , KC_F2  , KC_F3  , KC_F7  ,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                 KC_LGUI, KC_LSFT,    KC_F10, KC_F9
   ),
 // XXXX   4560
 // XXXX   1230
