a1a35ece553aa8d6a177a9407e31ef1bc68065c9
ivan plyusnin <ivan@plusn.in>
2026-05-16 18:24:43 +0500

    encoder

--- keyboards/cheapino/encoder.c
+++ keyboards/cheapino/encoder.c
@@ -1,5 +1,8 @@
 #include "matrix.h"
 #include "quantum.h"
+#ifdef CONSOLE_ENABLE
+#    include "print.h"
+#endif
 
 #define COL_SHIFTER ((uint16_t)1)
 
@@ -8,33 +11,87 @@
 #define ENC_B_COL 4
 #define ENC_BUTTON_COL 0
 
-static bool colABPressed   = false;
-static bool encoderPressed = false;
+static bool colABPressed              = false;
+static bool encoderPressed            = false;
+static bool encoderRotatedWhileHeld   = false;
 
 void clicked(void) {
+#ifdef CONSOLE_ENABLE
+    uprintln("enc: action=click -> KC_MPLY");
+#endif
     tap_code(KC_MPLY);
 }
 
 void turned(bool clockwise) {
     uint8_t mods = get_mods() | get_oneshot_mods();
+    uint16_t keycode;
 
-    if (mods & MOD_MASK_SHIFT) {
-        tap_code(clockwise ? KC_MPRV : KC_MNXT);
-    } else {
-        tap_code(clockwise ? KC_VOLD : KC_VOLU);
+    if (encoderPressed) {
+        encoderRotatedWhileHeld = true;
     }
+
+    if (encoderPressed) {
+        keycode = clockwise ? KC_MPRV : KC_MNXT;
+#ifdef CONSOLE_ENABLE
+        uprintf("enc: turn %s btn=1 shift=%u -> track %s\n", clockwise ? "CW" : "CCW", !!(mods & MOD_MASK_SHIFT), clockwise ? "MPRV" : "MNXT");
+#endif
+    } else if (mods & MOD_MASK_SHIFT) {
+        keycode = clockwise ? KC_MRWD : KC_MFFD;
+#ifdef CONSOLE_ENABLE
+        uprintf("enc: turn %s btn=0 shift=1 -> seek %s\n", clockwise ? "CW" : "CCW", clockwise ? "MRWD" : "MFFD");
+#endif
+    } else {
+        keycode = clockwise ? KC_VOLD : KC_VOLU;
+#ifdef CONSOLE_ENABLE
+        uprintf("enc: turn %s btn=0 shift=0 -> vol %s\n", clockwise ? "CW" : "CCW", clockwise ? "VOLD" : "VOLU");
+#endif
+    }
+    tap_code(keycode);
 }
 
 void fix_encoder_action(matrix_row_t current_matrix[]) {
     matrix_row_t encoder_row = current_matrix[ENC_ROW];
 
+#ifdef CONSOLE_ENABLE
+    static matrix_row_t last_encoder_row   = 0;
+    static bool         last_btn_pressed = false;
+    const bool          btn_pressed        = encoder_row & (COL_SHIFTER << ENC_BUTTON_COL);
+    if (encoder_row != last_encoder_row || btn_pressed != last_btn_pressed) {
+        uprintf("enc: row %04X btn_col%u=%u held=%u (bits: ", encoder_row, ENC_BUTTON_COL, btn_pressed, encoderPressed);
+        for (uint8_t b = 0; b < 12; b++) {
+            if (encoder_row & (COL_SHIFTER << b)) {
+                uprintf("%u ", b);
+            }
+        }
+        uprintln(")");
+        last_encoder_row   = encoder_row;
+        last_btn_pressed   = btn_pressed;
+    }
+#endif
+
     if (encoder_row & (COL_SHIFTER << ENC_BUTTON_COL)) {
+        if (!encoderPressed) {
+            encoderRotatedWhileHeld = false;
+#ifdef CONSOLE_ENABLE
+            uprintln("enc: btn down");
+#endif
+        }
         encoderPressed = true;
     } else {
-        // Only trigger click on release
+        // Click only on release without rotation while held
         if (encoderPressed) {
             encoderPressed = false;
-            clicked();
+#ifdef CONSOLE_ENABLE
+            if (encoderRotatedWhileHeld) {
+                uprintln("enc: btn up, rotated=1 -> click suppressed");
+            } else {
+                uprintln("enc: btn up, rotated=0 -> click");
+            }
+#endif
+            if (!encoderRotatedWhileHeld) {
+                clicked();
+            }
+            encoderRotatedWhileHeld = false;
         }
     }
 
